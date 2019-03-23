d93396a04dc2214a1ad55385e2f70e055f1bad14
airmrbiscuit <chrisandrews5@yahoo.co.uk>
2019-03-23 20:35:48 +0000

    Updates and adding RF material

--- Decision_Tree.R
+++ Decision_Tree.R
@@ -16,12 +16,14 @@ library('rpart')
 library('rattle') # rPart visuals
 library('rpart.plot') # rPart visuals
 library('RColorBrewer')# rPart visuals
+library('dplyr') # data manipulation
+library('ggplot2')
+library('ggthemes') # visualization
+
 
 #### Credit based analysis ####
 
 credit <- read.csv("credit.csv")
-
-table(credit$default)
 
 credit$default <- as.factor(credit$default)
 
@@ -38,33 +40,35 @@ credit_test <- credit_rand[901:1000,]
 
 
 #### Create Model ####
+
+
 # Ultimately all of these generate the same result on this dataset...
 # Need to add random forest & log reg (assume all the same outcome...)
 # Then apply to an alternative dataset (FPL?)
 
 
 
-# Remove the independent / target variable (column 17) & create model
+#1 Remove the independent / target variable (column 17) & create model
 
 credit_model <- C5.0(credit_train[-17], credit_train$default)
 
-# Limit the over-fitting by using minimum volume in each category
+#2 Limit the over-fitting by using minimum volume in each category
 
 credit_model <- C5.0(credit_train[-17], credit_train$default, control = C5.0Control(minCases = 20))
 
-# Use boosting - Create additional trees and use as vote, tend to be shallower and then weighted
+#3 Use boosting - Create additional trees and use as vote, tend to be shallower and then weighted
 # In this instance slightly better error ratio but many more non-predicted defaults (true negative)
 
 credit_model <- C5.0(credit_train[-17], credit_train$default, trials = 10)
 
-# Further option is error cost, make the true negative more penalised. It works to avoid those if they are more expensive, but do miss others
+#4 Further option is error cost, make the true negative more penalised. It works to avoid those if they are more expensive, but do miss others
 
 error_cost <- matrix(c(0,1,4,0), nrow = 2)
 
 credit_model <- C5.0(credit_train[-17], credit_train$default, costs = error_cost)
 
-# Use tree (its another CART) and uses all variables
-# Use cross_validation (multiple trees) and decide which has the lowest misclassifcation level
+#5 Use tree (its another CART) and uses all variables
+#6 Use cross_validation (multiple trees) and decide which has the lowest misclassifcation level
 
 credit_model <- tree(default~. , data = credit_train)
 
@@ -74,13 +78,13 @@ plot(credit_cv)
 credit_model = prune.misclass(credit_model, best = 8)
 
 
-# Alternative decision tree is rPart (recursive partition) - can choose method = class for classification or anova for regression (i.e. predict a number)
+#7 Alternative decision tree is rPart (recursive partition) - can choose method = class for classification or anova for regression (i.e. predict a number)
 
 credit_model <- rpart(as.factor(default) ~ checking_balance + credit_history + savings_balance + foreign_worker + months_loan_duration + other_debtors + job,
                 data=credit_train,
                 method="class")
 
-# rPart with a control for minimum entries on the tree
+#8 rPart with a control for minimum entries on the tree
 
 credit_model <- rpart(as.factor(default) ~ checking_balance + credit_history + savings_balance + foreign_worker + months_loan_duration + other_debtors + job,
                       data=credit_train,
@@ -88,6 +92,31 @@ credit_model <- rpart(as.factor(default) ~ checking_balance + credit_history + s
                       control=rpart.control( minsplit = 10 ))
                       
 
+#9 Random Forest
+
+credit_model <- randomForest(as.factor(default) ~ checking_balance + credit_history + savings_balance + foreign_worker + months_loan_duration + other_debtors + job,
+                      data=credit_train)
+
+
+
+
+#10 Series of random forests (mtry) which does generate the most correct
+
+credit_model <- randomForest(as.factor(default) ~ checking_balance + credit_history + savings_balance + foreign_worker + months_loan_duration + other_debtors + job,
+                             data=credit_train, mtry = 7)
+
+
+
+
+#11 Boosting grows smaller and stubbier trees - Gaussian distribution with squared error loss, 10000 shallow trees with 4 split in each, shrinkage is 0.01
+# equally good result as the multiple RF
+
+credit_model = gbm(default~. , data = credit_train, distribution = "gaussian", n.trees = 10000, shrinkage = 0.01, interaction.depth = 4)
+
+
+
+# Ideally would run a couple at once (which is what the pros are doing)
+# Explanation of the maths within the downloaded book
 
 
 
@@ -98,14 +127,17 @@ credit_model <- rpart(as.factor(default) ~ checking_balance + credit_history + s
 
 credit_model
 
-# Detailed summary of tree, the confusion matrix and key attributes
+# Detailed summary
 
 summary(credit_model)
 
 # Visual tree - not much detail on there really
+# Green lne for 2, red for 1 and black line is overall
 
 plot(credit_model)
 text(credit_model, pretty = 0)
+
+#plot(credit_model, ylim=c(0,0.36)) # alternative view
 
 # Much More attractive tree (but only for rpart)
 
@@ -116,6 +148,35 @@ fancyRpartPlot(credit_model)
 credit_model <- prp(credit_model,snip=TRUE)$obj
 
 fancyRpartPlot(credit_model)
+
+# View factor significance (random forest)
+
+importance    <- randomForest::importance(credit_model)
+
+varImportance <- data.frame(Variables = row.names(importance), 
+                            Importance = round(importance[ ,'MeanDecreaseGini'],2))
+
+# Create a rank variable based on importance
+rankImportance <- varImportance %>%
+  mutate(Rank = paste0('#',dense_rank(desc(Importance))))
+
+
+# Use ggplot2 to visualize the relative importance of variables
+ggplot(rankImportance, aes(x = reorder(Variables, Importance), 
+                           y = Importance, fill = Importance)) +
+  geom_bar(stat='identity') + 
+  geom_text(aes(x = Variables, y = 0.5, label = Rank),
+            hjust=0, vjust=0.55, size = 4, colour = 'red') +
+  labs(x = 'Variables') +
+  coord_flip() + 
+  theme_few()
+
+legend('topright', colnames(credit_model$err.rate), col=1:3, fill=1:3)
+
+
+
+
+
 
 
 #### Using to Predict ####
@@ -129,6 +190,7 @@ credit_pred <- predict(credit_model, credit_test)
 CrossTable(credit_test$default, credit_pred,
            prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE,
            dnn = c('actual default', 'predicted default'))
+
 
 
 
